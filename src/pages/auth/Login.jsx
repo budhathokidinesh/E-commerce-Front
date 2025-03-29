8e582d7158fe63ff10b78f86d490501bab489fa8
Dinesh Budhathoki <physmarika@gmail.com>
2025-03-29 10:35:37 +0800

    Register and login page setup is finished

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,7 +1,41 @@
-import React from "react";
+import CommonForm from "@/components/common/form";
+import { loginFormControls } from "@/config/config";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+const initialState = {
+  email: "",
+  password: "",
+};
 
 const Login = () => {
-  return <div>login</div>;
+  const [formData, setFormData] = useState(initialState);
+  const onSubmit = () => {};
+  return (
+    <div className="mx-auto w-full max-w-md space-y-6">
+      <div className="text-center">
+        <h1 className="text-3xl font-bold tracking-tight text-foreground">
+          Log In Now
+        </h1>
+        <p className="mt-2">
+          Don't have account. Please register now. &nbsp;
+          <Link
+            className="font-medium text-primary hover:underline hover:text-blue-700"
+            to="/register"
+          >
+            Register
+          </Link>
+        </p>
+      </div>
+      <CommonForm
+        formControls={loginFormControls}
+        buttonText={"Log In"}
+        formData={formData}
+        setFormData={setFormData}
+        onSubmit={onSubmit}
+      />
+    </div>
+  );
 };
 
 export default Login;
--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -1,7 +1,42 @@
-import React from "react";
+import CommonForm from "@/components/common/form";
+import { registerFormControls } from "@/config/config";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+const initialState = {
+  name: "",
+  email: "",
+  password: "",
+};
 
 const Register = () => {
-  return <div>Register</div>;
+  const [formData, setFormData] = useState(initialState);
+  const onSubmit = () => {};
+  return (
+    <div className="mx-auto w-full max-w-md space-y-6">
+      <div className="text-center">
+        <h1 className="text-3xl font-bold tracking-tight text-foreground">
+          Sign Up Now
+        </h1>
+        <p className="mt-2">
+          Already have an account &nbsp;
+          <Link
+            className="font-medium text-primary hover:underline hover:text-blue-700"
+            to="/login"
+          >
+            Login
+          </Link>
+        </p>
+      </div>
+      <CommonForm
+        formControls={registerFormControls}
+        buttonText={"Sign Up"}
+        formData={formData}
+        setFormData={setFormData}
+        onSubmit={onSubmit}
+      />
+    </div>
+  );
 };
 
 export default Register;
